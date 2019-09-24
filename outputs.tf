8350381194b2d36de402fcbe650ca2b930077f01
Nikita Dugar <nikita@clouddrove.com>
2019-09-24 10:40:36 +0530

    change output syntax

--- outputs.tf
+++ outputs.tf
@@ -2,9 +2,7 @@
 #Description : This terraform module creates set of Security Group and Security Group Rules
 #              resources in various combinations.
 output "security_group_ids" {
-  value = concat(
-    aws_security_group.default.*.id
-  )[0]
+  value = join("", aws_security_group.default.*.id)
   description = "IDs on the AWS Security Groups associated with the instance."
 }
 
