4bfaaeb255d1c54c9bdc84e40d1d4706f8c93ef9
Sohan <31764946+sohanyadav@users.noreply.github.com>
2019-12-27 15:51:03 +0530

    github action (#1)
    * github action
    
    * github action

--- outputs.tf
+++ outputs.tf
@@ -2,7 +2,7 @@
 #Description : This terraform module creates set of Security Group and Security Group Rules
 #              resources in various combinations.
 output "security_group_ids" {
-  value = join("", aws_security_group.default.*.id)
+  value       = join("", aws_security_group.default.*.id)
   description = "IDs on the AWS Security Groups associated with the instance."
 }
 
