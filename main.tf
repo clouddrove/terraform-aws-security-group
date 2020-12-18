bd637ae536d03fd26e7ae80a9c3e73262baeb9b8
Ajay Kumar Dhyani <ajaykumardhyani@gmail.com>
2020-12-18 15:30:08 +0530

    Upgrade terraform version 0.14

--- main.tf
+++ main.tf
@@ -8,10 +8,10 @@
 #              convention.
 
 module "labels" {
-  source = "git::https://github.com/clouddrove/terraform-labels.git?ref=tags/0.13.0"
+  source = "git::https://github.com/clouddrove/terraform-labels.git?ref=tags/0.14.0"
 
   name        = var.name
-  application = var.application
+  repository  = var.repository
   environment = var.environment
   managedby   = var.managedby
   label_order = var.label_order
