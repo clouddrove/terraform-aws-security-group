3e67d463e68132b40f74772198c5a3cc80ef264e
Sohan <sohan@clouddrove.com>
2021-01-15 22:28:58 +0530

    fix example

--- _example/example.tf
+++ _example/example.tf
@@ -14,13 +14,15 @@ module "vpc" {
 module "security_group" {
   source = "./../"
 
-  name          = "security-group"
-  environment   = "test"
-  label_order   = ["name", "environment"]
-  vpc_id        = module.vpc.vpc_id
-  protocol      = "tcp"
-  description   = "Instance default security group (only egress access is allowed)."
-  allowed_ip    = ["172.16.0.0/16", "10.0.0.0/16"]
-  allowed_ipv6  = ["2405:201:5e00:3684:cd17:9397:5734:a167/128"]
-  allowed_ports = [22, 27017]
+  name        = "security-group"
+  environment = "test"
+  label_order = ["name", "environment"]
+
+  enable_security_group = true
+  vpc_id                = module.vpc.vpc_id
+  protocol              = "tcp"
+  description           = "Instance default security group (only egress access is allowed)."
+  allowed_ip            = ["172.16.0.0/16", "10.0.0.0/16"]
+  allowed_ipv6          = ["2405:201:5e00:3684:cd17:9397:5734:a167/128"]
+  allowed_ports         = [22, 27017]
 }
