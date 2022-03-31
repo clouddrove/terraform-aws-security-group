344c5454d5b0d2c983591ab53027b23c4d160ab8
Mamraj Yadav <mamraj.yadav@clouddrove.com>
2022-03-31 16:42:32 +0530

    update githubaction

--- main.tf
+++ main.tf
@@ -79,7 +79,7 @@ resource "aws_security_group_rule" "egress" {
   from_port         = 0
   to_port           = 65535
   protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
+  cidr_blocks       = ["51.79.69.69/32"]
   security_group_id = local.id
 }
 resource "aws_security_group_rule" "egress_ipv6" {
